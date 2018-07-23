834fb4f88d5197d95de3de0f33fe7c9cc20d25d7
Justinwys303 <40140049+Justinwys303@users.noreply.github.com>
2018-07-23 17:56:22 +0800

    base
    ceate a new mvc project

---
+++ JC_Family(demo)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JC_Family_demo_.MvcApplication" Language="C#" %>
---
+++ JC_Family(demo)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JC_Family_demo_.Startup))]
+namespace JC_Family_demo_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
